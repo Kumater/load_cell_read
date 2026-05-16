c3c999ce4cda4af36004fdb8b56c2d22271bff90
Kumater <kumaterpeter@gmail.com>
2026-05-16 16:37:58 +0200

    spi test

--- spi_test.py
+++ spi_test.py
@@ -1,10 +1,21 @@
 import spidev
+import time
+
 spi = spidev.SpiDev()
 spi.open(0, 0)
-spi.max_speed_hz = 100000
+spi.max_speed_hz = 100000  # slow so multimeter can catch it
 spi.mode = 0
 
-result = spi.xfer2([0xAA, 0x55, 0xFF])
-print([hex(x) for x in result])
-# Should print: ['0xaa', '0x55', '0xff']
-# If you get all 0x00, SPI itself is broken
+print("Starting continuous read loop - probe pins now...")
+print("Press Ctrl+C to stop")
+print("-" * 50)
+
+try:
+    while True:
+        raw = spi.xfer2([0x00] * 7)
+        print([hex(x) for x in raw])
+        time.sleep(0.05)
+
+except KeyboardInterrupt:
+    print("\nStopped.")
+    spi.close()
