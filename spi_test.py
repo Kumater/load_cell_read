e461f340bb1e254e01fa2ee15df2f463ba6c711f
Kumater <kumaterpeter@gmail.com>
2026-05-16 16:56:09 +0200

    spi test

--- spi_test.py
+++ spi_test.py
@@ -2,20 +2,15 @@ import spidev
 import time
 
 spi = spidev.SpiDev()
-spi.open(0, 0)
-spi.max_speed_hz = 100000  # slow so multimeter can catch it
+spi.open(0, 1)  # <-- changed to CE1
+spi.max_speed_hz = 100000
 spi.mode = 0
 
-print("Starting continuous read loop - probe pins now...")
-print("Press Ctrl+C to stop")
-print("-" * 50)
-
+print("Trying spidev0.1 ...")
 try:
     while True:
         raw = spi.xfer2([0x00] * 7)
         print([hex(x) for x in raw])
         time.sleep(0.05)
-
 except KeyboardInterrupt:
-    print("\nStopped.")
     spi.close()
